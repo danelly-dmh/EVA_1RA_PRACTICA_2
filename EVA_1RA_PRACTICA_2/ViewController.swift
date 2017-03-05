24ebaba49ee37ca96e8ce84f155035f43d55fef0
danelly-dmh <danelly.dmh@gmail.com>
2017-03-04 20:07:29 -0700

    1raEvaPractica2

---
+++ EVA_1RA_PRACTICA_2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  EVA_1RA_PRACTICA_2
+//
+//  Created by TEMPORAL2 on 16/02/17.
+//  Copyright © 2017 comdanelly. All rights reserved.
+// <Hola mundo>
+// written by: DANELLY MONTAÑEZ HERNÁNDEZ
+// <INSTITUTO TECNOLÓGICO DE CHIHUAHUA II>
+// <Desarrollo de Aplicaciones para Dispositivos Móviles, Plataforma 1>
+// <03-05-2017>
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
